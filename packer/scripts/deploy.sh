d1a44fb734df4d10c2b99be47931047f12b9c88f
Mikhail Androsov <bambarambambum@yandex.ru>
2020-03-24 19:44:26 +0300

    Fixed deploy.sh

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -4,8 +4,6 @@ echo "Download app..."
 git clone -b monolith https://github.com/express42/reddit.git
 echo "Install bundle..."
 cd reddit && bundle install
-mv ../start.sh .
-mv ../stop.sh .
 sudo mv ../myapp.service /etc/systemd/system/
 echo "Start app..."
 sudo systemctl enable myapp.service
