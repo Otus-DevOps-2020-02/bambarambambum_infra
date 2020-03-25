6dd0a73508f3bf67db7b7cc6e1cedab53832bb60
Mikhail Androsov <bambarambambum@yandex.ru>
2020-03-25 23:03:37 +0300

    First task complete

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,10 @@ provider "google" {
   region = var.region
 }
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
-  tags = ["reddit-app"]
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
   boot_disk {
     initialize_params {
       image = var.disk_image
@@ -21,34 +21,34 @@ resource "google_compute_instance" "app" {
   }
   metadata = {
     ssh-keys = "mikh_androsov:${file(var.public_key_path)}"
-    }
+  }
 
   network_interface {
     network = "default"
     access_config {}
   }
-connection {
-  type = "ssh"
-  host = self.network_interface[0].access_config[0].nat_ip
-  user = "mikh_androsov"
-  agent = false
-  private_key = file("~/.ssh/mikh_androsov")
-}
-    provisioner "file" {
-source = "files/puma.service"
-destination = "/tmp/puma.service"
+  connection {
+    type        = "ssh"
+    host        = self.network_interface[0].access_config[0].nat_ip
+    user        = "mikh_androsov"
+    agent       = false
+    private_key = file("~/.ssh/mikh_androsov")
   }
-provisioner "remote-exec" {
-script = "files/deploy.sh"
-}
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
+  target_tags   = ["reddit-app"]
 }
