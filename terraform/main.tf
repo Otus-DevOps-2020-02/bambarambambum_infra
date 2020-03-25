a0293b18b85f154896a0952895275dd9960381a8
Mikhail Androsov <bambarambambum@yandex.ru>
2020-03-25 22:51:11 +0300

    Study and preparation of terraform

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_version = "0.12.8"
+}
+
+provider "google" {
+  version = "2.15"
+
+  project = var.project
+
+  region = var.region
+}
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+  tags = ["reddit-app"]
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+  metadata = {
+    ssh-keys = "mikh_androsov:${file(var.public_key_path)}"
+    }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+connection {
+  type = "ssh"
+  host = self.network_interface[0].access_config[0].nat_ip
+  user = "mikh_androsov"
+  agent = false
+  private_key = file("~/.ssh/mikh_androsov")
+}
+    provisioner "file" {
+source = "files/puma.service"
+destination = "/tmp/puma.service"
+  }
+provisioner "remote-exec" {
+script = "files/deploy.sh"
+}
+}
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["reddit-app"]
+}
