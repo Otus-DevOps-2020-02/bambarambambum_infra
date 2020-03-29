7b08edfe70ceb7d0484a022f79f91431f1d35926
Mikhail Androsov <bambarambambum@yandex.ru>
2020-03-29 17:44:53 +0300

    Renamed lb.tf_1 to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,16 @@ terraform {
 
 provider "google" {
   version = "2.15"
-
   project = var.project
-
-  region = var.region
+  region  = var.region
 }
+
+// My instansces
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
+  count        = 2
+  name         = "reddit-app-${count.index}"
   machine_type = "g1-small"
-  zone         = "europe-west1-b"
+  zone         = var.zone
   tags         = ["reddit-app"]
   boot_disk {
     initialize_params {
@@ -27,17 +28,20 @@ resource "google_compute_instance" "app" {
     network = "default"
     access_config {}
   }
+
   connection {
     type        = "ssh"
     host        = self.network_interface[0].access_config[0].nat_ip
     user        = "mikh_androsov"
     agent       = false
-    private_key = file("~/.ssh/mikh_androsov")
+    private_key = file(var.private_key_path)
   }
+
   provisioner "file" {
     source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
+
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
@@ -53,8 +57,35 @@ resource "google_compute_firewall" "firewall_puma" {
   target_tags   = ["reddit-app"]
 }
 
-// SSH Key
+// SSH Keys
 resource "google_compute_project_metadata_item" "ssh-keys" {
   key   = "ssh-keys"
   value = "${join("\n", var.public_key)}"
+}
+
+// 9292 check
+resource "google_compute_http_health_check" "tcp-9292-connect" {
+  name               = "tcp-9292-connect"
+  description        = "Check app health"
+  request_path       = "/"
+  port               = var.port_app
+  check_interval_sec = 10
+  timeout_sec        = 5
+}
+
+// Resource pool
+resource "google_compute_target_pool" "app-pool" {
+  name      = "app-pool"
+  region    = var.region
+  instances = google_compute_instance.app.*.self_link
+  health_checks = [
+    google_compute_http_health_check.tcp-9292-connect.name
+  ]
+}
+
+// Forwarding rule
+resource "google_compute_forwarding_rule" "lb" {
+  name       = "app-pool-lb"
+  target     = google_compute_target_pool.app-pool.self_link
+  port_range = "9292"
 }
